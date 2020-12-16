bee447d65fa79403d674cad8f26c890f8e45aa1d
devanshu-jotwani <devanshujotwani14@gmail.com>
2020-12-16 16:48:22 +0530

    django initial changes

--- devnikk/devnikk/urls.py
+++ devnikk/devnikk/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from devanshunikket.views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
